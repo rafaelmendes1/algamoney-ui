e88226babeed342b0995ef51dc9daf09fec5573b
rafaelmendes1 <rarafafa3245@gmail.com>
2020-08-21 17:29:59 -0300

    Adicionando tooltip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,7 @@ import { AppComponent } from './app.component';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
-
-
+import { TooltipModule } from 'primeng/tooltip';
 
 @NgModule({
   declarations: [
@@ -19,6 +18,7 @@ import { TableModule } from 'primeng/table';
     InputTextModule,
     ButtonModule,
     TableModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
