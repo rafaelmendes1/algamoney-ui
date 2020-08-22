45b68d4ce7712b13361ef247842233eb2e70e412
rafaelmendes1 <rarafafa3245@gmail.com>
2020-08-22 11:20:19 -0300

    Adicionando pipes

--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -13,8 +13,8 @@ export class LancamentosPesquisaComponent implements OnInit {
   }
 
   lancamentos = [
-    { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017', dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José'},
-    { tipo: 'RECEITA', descricao: 'Softwares', dataVencimento: '30/09/2017', dataPagamento: '30/03/2017', valor: 100000, pessoa: 'Time 7'}
+    { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: new Date(2017, 6, 30), dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José'},
+    { tipo: 'RECEITA', descricao: 'Softwares', dataVencimento: new Date(2018, 3 ,6), dataPagamento: new Date(2017, 12, 6), valor: 100000, pessoa: 'Time 7'}
   ]
   
 }
