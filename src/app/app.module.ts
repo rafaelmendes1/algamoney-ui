11f7d8280b95aafe41ab33fa581789467e97b3a7
rafaelmendes1 <rarafafa3245@gmail.com>
2020-08-25 18:49:54 -0300

    Criando componente de mensagem de erro de validação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
 import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
 import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+import { MessageComponent } from './message/message.component';
 
 
 
@@ -33,7 +34,8 @@ import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.compo
     NavbarComponent,
     PessoasPesquisaComponent,
     LancamentoCadastroComponent,
-    PessoaCadastroComponent
+    PessoaCadastroComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/message/message.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-message',
+  template: `
+    <div *ngIf="temErro()" class="ng-dirty p-invalid ng-invalid">
+      <p-message severity="error" text="{{ text }}"></p-message>
+    </div>
+  `,
+  styles: [
+  ]
+})
+export class MessageComponent {
+
+  @Input() error: string;
+  @Input() control: FormControl;
+  @Input() text: string;
+
+  temErro(): boolean {
+    return this.control.hasError(this.error) && this.control.dirty;
+  }
+}
