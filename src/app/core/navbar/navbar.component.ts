5178cca68cce2f3ab94048d56d6d41b28b673a5f
rafaelmendes1 <rarafafa3245@gmail.com>
2020-09-22 11:07:19 -0300

    Obtendo um novo access token

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -15,4 +15,7 @@ export class NavbarComponent implements OnInit {
 
   exibindoMenu = false;
 
+  criarNovoAccessToken() {
+    this.auth.obterNovoAccessToken();
+  }
 }
--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -22,7 +22,7 @@ export class AuthService {
 
     const body = `username=${usuario}&password=${senha}&grant_type=password`;
 
-    return this.http.post(this.oauthTokenUrl, body, { headers })
+    return this.http.post(this.oauthTokenUrl, body, { headers, withCredentials: true })
     .toPromise()
     .then(response => {
       this.armazenarToken(response.json().access_token);
@@ -37,6 +37,28 @@ export class AuthService {
       }
 
       return Promise.reject(response);
+    });
+  }
+
+  obterNovoAccessToken(): Promise<void> {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    headers.append('Authorization', 'Basic YW5ndWxhcjpAbmd1MWFy');
+
+    const body = 'grant_type=refresh_token';
+
+    return this.http.post(this.oauthTokenUrl, body, { headers, withCredentials: true })
+    .toPromise()
+    .then(response => {
+      this.armazenarToken(response.json().access_token);
+      console.log("Novo token criado!")
+
+      return Promise.resolve(null);
+    })
+    .catch(response => {
+      console.error('Erro ao renovar token.', response);
+
+      return Promise.resolve(null);
     });
   }
 
